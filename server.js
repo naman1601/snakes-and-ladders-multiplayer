88787676e8343e3c4a2394de5b7e13fc4f236509
naman1601 <namanchhaparia1601@gmail.com>
2023-08-05 19:18:25 +0530

    Set up server and connect to server on front-end

--- server.js
+++ server.js
@@ -6,37 +6,15 @@ const app = express();
 const PORT = 3000 || process.env.PORT;
 const server = http.createServer(app);
 
-// Set static folder
 app.use(express.static("public"));
 
-// Socket setup
+// SOCKET SETUP
 const io = socket(server);
 
-// Players array
-let users = [];
-
+// SOCKET EVENTS
 io.on("connection", (socket) => {
   console.log("Made socket connection", socket.id);
-
-  socket.on("join", (data) => {
-    users.push(data);
-    io.sockets.emit("join", data);
-  });
-
-  socket.on("joined", () => {
-    socket.emit("joined", users);
-  });
-
-  socket.on("rollDice", (data) => {
-    users[data.id].pos = data.pos;
-    const turn = data.num != 6 ? (data.id + 1) % users.length : data.id;
-    io.sockets.emit("rollDice", data, turn);
-  });
-
-  socket.on("restart", () => {
-    users = [];
-    io.sockets.emit("restart");
-  });
 });
 
+// SERVER LISTENING
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
